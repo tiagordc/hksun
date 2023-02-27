fb62b5ce0400f1d44d3fecc28c92c961077a9772
Tiago Costa <tiagorc+gitlab@gmail.com>
2023-02-27 15:54:46 +0000

    reset

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import asyncio, os, sqlite3, datetime, sys
 from huawei_solar import HuaweiSolarBridge, register_names as rn
-from quart import Quart
+from quart import Quart, abort
 
 # if POWER_METER_ACTIVE_POWER < 0 -> GRID CONSUMPTION
 
@@ -86,43 +86,70 @@ async def loop():
 @app.before_serving
 async def startup(): app.add_background_task(loop)
 
-@app.route("/average/<int:minutes>")
-async def average(minutes):
-    conn, close = database()
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM [Inverter] WHERE [Fault] = 0 AND [Timestamp] > datetime('now', '-{minutes} minutes') ORDER BY [Timestamp]")
-    rows = cur.fetchall()
-    if close: conn.close()
-    result = { 'active': 0.0, 'meter': 0.0, 'pv1': 0.0, 'pv2': 0.0, 'temp': 0.0, 'import': False, 'export': False, 'inported': 0.0, 'exported': 0.0, 'consumed': 0.0, 'produced': 0.0, 'consumption': 0.0 }
+def format_data(rows):
+    result = { 'active': 0.0, 'meter': 0.0, 'pv1': 0.0, 'pv2': 0.0, 'temp': 0.0, 'inported': 0.0, 'exported': 0.0, 'consumed': 0.0, 'produced': 0.0, 'consumption': 0.0, 'import': False, 'export': False, 'faults': 0 }
     for row in rows:
-        result['active'] += row[0]
-        result['meter'] += row[1]
-        result['pv1'] += row[2]
-        result['pv2'] += row[3]
-        result['temp'] += row[4]
-        if row[1] < 0: result['import'] = True
-        elif row[1] > 0: result['export'] = True
+        if row[5] != 0: 
+            result['faults'] += 1
+        else:
+            result['active'] += row[0]
+            result['meter'] += row[1]
+            result['pv1'] += row[2]
+            result['pv2'] += row[3]
+            result['temp'] += row[4]
+            if row[1] < 0: 
+                result['import'] = True
+            elif row[1] > 0: 
+                result['export'] = True
     if any(rows):
         l = len(rows)
-        result['active'] /= l
-        result['meter'] /= l
-        result['pv1'] /= l
-        result['pv2'] /= l
-        result['temp'] /= l
+        result['active'] = round(result['active'] / l, 2)
+        result['meter'] = round(result['meter'] / l, 2)
+        result['pv1'] = round(result['pv1'] / l, 2)
+        result['pv2'] = round(result['pv2'] / l, 2)
+        result['temp'] = round(result['temp'] / l, 2)
         f, l = rows[0], rows[-1]
         solar_production = l[6] - f[6]
         solar_to_grid = l[7] - f[7]
         solar_to_house = solar_production - solar_to_grid
         grid_to_house = l[8] - f[8]
         house_consumption = grid_to_house + solar_to_house
-        if solar_to_grid > 0: result['export'] = True
-        if solar_to_house > 0: result['import'] = True
-        result['inported'] = grid_to_house
-        result['exported'] = solar_to_grid
-        result['consumed'] = solar_to_house
-        result['produced'] = solar_production
-        result['consumption'] = house_consumption
+        if solar_to_grid > 0: 
+            result['export'] = True
+        if solar_to_house > 0: 
+            result['import'] = True
+        result['inported'] = round(grid_to_house, 2)
+        result['exported'] = round(solar_to_grid, 2)
+        result['consumed'] = round(solar_to_house, 2)
+        result['produced'] = round(solar_production, 2)
+        result['consumption'] = round(house_consumption, 2)
     return result
+
+@app.get("/average/<int:minutes>")
+async def average(minutes):
+    conn, close = database()
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM [Inverter] WHERE [Timestamp] > datetime('now', '-{minutes} minutes') ORDER BY [Timestamp]")
+    rows = cur.fetchall()
+    if close: conn.close()
+    return format_data(rows)
+
+@app.get("/reset")
+async def reset():
+    if sys.platform == 'darwin': return 'OK'
+    os.system('sudo ifdown wlan0')
+    await asyncio.sleep(1)
+    os.system('sudo ifup --force wlan0')
+    await asyncio.sleep(1)
+    now = datetime.datetime.now()
+    while True:
+        try:
+            ret = os.system(f'ping -c 1 {INVERTER}')
+            if ret == 0: break
+        except: pass
+        if (datetime.datetime.now() - now).seconds > 60: abort(500, 'Could not connect to inverter')
+        await asyncio.sleep(1)
+    return 'OK' 
 
 if __name__ == "__main__":
     app.run()

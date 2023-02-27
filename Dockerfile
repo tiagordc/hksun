ee0dfed58ef7fa7b38c2cd723b51a0151a3c8a80
Tiago Costa <tiagorc+gitlab@gmail.com>
2023-02-27 13:17:24 +0000

    rest api

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ RUN mkdir /app
 WORKDIR /app
 RUN pip install --upgrade pip
 RUN pip install huawei-solar==2.2.4
+RUN pip install apscheduler==3.10.0
+RUN pip install quart==0.18.3
 COPY app.py /app
 CMD [ "python", "-u", "app.py" ]
--- app.py
+++ app.py
@@ -1,35 +1,60 @@
-import asyncio, os, sqlite3, datetime
+import asyncio, os, sqlite3, datetime, sys
 from huawei_solar import HuaweiSolarBridge, register_names as rn
+from quart import Quart
+
+# if POWER_METER_ACTIVE_POWER < 0 -> GRID CONSUMPTION
 
 INVERTER = '192.168.200.1'
 DATA_PATH = '/database'
 DATA_BASE = 'readings.db'
 
-if not os.path.exists(DATA_PATH):
-    os.mkdir(DATA_PATH)
-
-def init_tables():
-    conn = sqlite3.connect(os.path.join(DATA_PATH, DATA_BASE))
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS [Inverter] ([Active] INTEGER, [Meter] INTEGER, [PV1] INTEGER, [PV2] INTEGER, [Temperature] REAL, \
-                   [Fault] INTEGER, [Yielded] REAL, [Exported] REAL, [Total] REAL, [Timestamp] DATETIME DEFAULT CURRENT_TIMESTAMP )')
-    conn.commit()
-    conn.close()
-
-init_tables()
+app = Quart(__name__)
 bridge = None
 
-async def connect():
-    global bridge
-    try:
-        bridge = await HuaweiSolarBridge.create(host=INVERTER, port=6607)
-    except Exception as e:
-        bridge = None
-        print(f'Error connecting to inverter: {e}')
+@app.route("/")
+async def ping(): return 'OK'
+
+if sys.platform == 'darwin': # this machine is not on the same network as the inverter
+
+    def database():
+        if '_db' in globals(): return globals()['_db'], False
+        _db = sqlite3.connect(':memory:')
+        globals()['_db'] = _db
+        return _db, False
+    
+    async def connect():
+        pass
+
+else: # running inside docker and with access to the inverter
+
+    if not os.path.exists(DATA_PATH):
+        os.mkdir(DATA_PATH)
+
+    def database():
+        return sqlite3.connect(os.path.join(DATA_PATH, DATA_BASE)), True
+
+    async def connect():
+        global bridge
+        try:
+            bridge = await HuaweiSolarBridge.create(host=INVERTER, port=6607)
+        except Exception as e:
+            bridge = None
+            print(f'Error connecting to inverter: {e}')
+
+def init_tables():
+    conn, close = database()
+    conn.execute('CREATE TABLE IF NOT EXISTS [Inverter] ([Active] INTEGER, [Meter] INTEGER, [PV1] INTEGER, [PV2] INTEGER, [Temperature] REAL, [Fault] INTEGER, [Yielded] REAL, [Exported] REAL, [Total] REAL, [Timestamp] DATETIME DEFAULT CURRENT_TIMESTAMP )')
+    conn.commit()
+    if close: conn.close()
+
+init_tables()
 
 async def read():
-    conn = sqlite3.connect(os.path.join(DATA_PATH, DATA_BASE))
-    if bridge is None: 
+    conn, close = database()
+    if bridge is None: await connect()
+    if sys.platform == 'darwin':
+        conn.execute("INSERT INTO [Inverter] ([Active], [Meter], [PV1], [PV2], [Temperature], [Fault], [Yielded], [Exported], [Total]) VALUES (?,?,?,?,?,?,?,?,?)", (1000, 500, 300, 700, 28.5, 0, 100.0, 50.0, 150.0))
+    elif bridge is None:
         conn.execute("INSERT INTO [Inverter] ([Fault]) VALUES (-1)")
     else:
         data = await bridge.update()
@@ -42,24 +67,62 @@ async def read():
         yielded = float(data[rn.ACCUMULATED_YIELD_ENERGY].value)
         exported = float(data[rn.GRID_EXPORTED_ENERGY].value)
         total = float(data[rn.GRID_ACCUMULATED_ENERGY].value)
-        conn.execute("INSERT INTO [Inverter] ([Active], [Meter], [PV1], [PV2], [Temperature], [Fault], [Yielded], [Exported], [Total]) \
-                     VALUES (?,?,?,?,?,?,?,?,?)", (active, meter, pv1, pv2, temp, fault, yielded, exported, total))
+        conn.execute("INSERT INTO [Inverter] ([Active], [Meter], [PV1], [PV2], [Temperature], [Fault], [Yielded], [Exported], [Total]) VALUES (?,?,?,?,?,?,?,?,?)", (active, meter, pv1, pv2, temp, fault, yielded, exported, total))
     conn.commit()
-    conn.close()
-    # if POWER_METER_ACTIVE_POWER < 0 -> GRID CONSUMPTION
+    if close: conn.close()
 
 async def loop():
     while True:
-        if bridge is None: 
-            await connect()
         try:
             await read()
         except Exception as e:
             print(f'Error reading inverter: {e}')
-            await connect()
+            bridge = None
         if 6 <= datetime.datetime.now().hour < 21: # In Portugal, the sun is up from 6am to 9pm at most
             await asyncio.sleep(15) # 15 seconds
         else:
             await asyncio.sleep(60 * 15) # 15 minutes
 
-asyncio.run(loop())
+@app.before_serving
+async def startup(): app.add_background_task(loop)
+
+@app.route("/average/<int:minutes>")
+async def average(minutes):
+    conn, close = database()
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM [Inverter] WHERE [Fault] = 0 AND [Timestamp] > datetime('now', '-{minutes} minutes') ORDER BY [Timestamp]")
+    rows = cur.fetchall()
+    if close: conn.close()
+    result = { 'active': 0.0, 'meter': 0.0, 'pv1': 0.0, 'pv2': 0.0, 'temp': 0.0, 'import': False, 'export': False, 'inported': 0.0, 'exported': 0.0, 'consumed': 0.0, 'produced': 0.0, 'consumption': 0.0 }
+    for row in rows:
+        result['active'] += row[0]
+        result['meter'] += row[1]
+        result['pv1'] += row[2]
+        result['pv2'] += row[3]
+        result['temp'] += row[4]
+        if row[1] < 0: result['import'] = True
+        elif row[1] > 0: result['export'] = True
+    if any(rows):
+        l = len(rows)
+        result['active'] /= l
+        result['meter'] /= l
+        result['pv1'] /= l
+        result['pv2'] /= l
+        result['temp'] /= l
+        f, l = rows[0], rows[-1]
+        solar_production = l[6] - f[6]
+        solar_to_grid = l[7] - f[7]
+        solar_to_house = solar_production - solar_to_grid
+        grid_to_house = l[8] - f[8]
+        house_consumption = grid_to_house + solar_to_house
+        if solar_to_grid > 0: result['export'] = True
+        if solar_to_house > 0: result['import'] = True
+        result['inported'] = grid_to_house
+        result['exported'] = solar_to_grid
+        result['consumed'] = solar_to_house
+        result['produced'] = solar_production
+        result['consumption'] = house_consumption
+    return result
+
+if __name__ == "__main__":
+    app.run()
